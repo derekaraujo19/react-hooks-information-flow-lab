fb5dae0ae8907b73ea07faf5c17510454ebaaa6a
Derek Araujo <derekaraujo19@gmail.com>
2021-11-02 17:45:22 -0400

    Assignment Complete

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header({ onDarkModeClick, buttonText }) {
+  return (
+    <header>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick}>
+      {buttonText ? "Dark" : "Light"} Mode
+    </button>
+  </header>
+  );
+}
+
+export default Header;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -14,15 +11,16 @@ function ShoppingList({ items }) {
     return item.category === selectedCategory;
   });
 
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+        <Filter onCategoryChange={handleCategoryChange}/>
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
